f04298fc8bb3b72718caf8d69079fa61eb95f70e
Ivan Muhammad Nizar <ivannizar@gmail.com>
2021-12-28 17:53:52 +0700

    comments

---
+++ features/comments/entity.go
@@ -0,0 +1,16 @@
+package comments
+
+type Core struct {
+	ID        int
+	Comment   string
+	ThreadID  int
+	UserID    int
+	ImageUrl  string
+	CommentID int
+}
+
+type Bussiness interface {
+}
+
+type Data interface {
+}
