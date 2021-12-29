d77f7a56db1f8800b58e98b66d3d5f0b04fdf971
Ivan Muhammad Nizar <ivannizar@gmail.com>
2021-12-29 17:14:52 +0700

    add 3 comments

--- features/threads/data/mysql.go
+++ features/threads/data/mysql.go
@@ -1,11 +1,1 @@
 package data
-
-type Thread struct {
-	ID            int
-	Title         string
-	Description   string
-	UserID        int
-	Like          int
-	JumlahComment int
-	ImgUrl        string
-}
--- migrate/migrate.go
+++ migrate/migrate.go
@@ -2,6 +2,7 @@ package migrate
 
 import (
 	"github.com/dragranzer/capstone-BE-FGD/config"
+	_comment_data "github.com/dragranzer/capstone-BE-FGD/features/comments/data"
 	_thread_data "github.com/dragranzer/capstone-BE-FGD/features/threads/data"
 	_user_data "github.com/dragranzer/capstone-BE-FGD/features/users/data"
 	"golang.org/x/crypto/bcrypt"
@@ -21,7 +22,11 @@ func AutoMigrate() {
 		panic(err)
 	}
 
-	config.DB.AutoMigrate(&_user_data.User{}, &_thread_data.Thread{})
+	if err := config.DB.Exec("DROP TABLE IF EXISTS comments").Error; err != nil {
+		panic(err)
+	}
+
+	config.DB.AutoMigrate(&_user_data.User{}, &_thread_data.Thread{}, &_comment_data.Comment{})
 
 	pass1, _ := HashPassword("pass1")
 	user1 := _user_data.User{
@@ -56,6 +61,25 @@ func AutoMigrate() {
 		UserID:      1,
 	}
 
+	comment1 := _comment_data.Comment{
+		Comment:  "GO emang didesain seperti itu gan",
+		UserID:   2,
+		ThreadID: 1,
+	}
+
+	comment2 := _comment_data.Comment{
+		Comment:   "Jawaban yang tidak menjawab",
+		UserID:    3,
+		ThreadID:  1,
+		CommentID: 1,
+	}
+
+	comment3 := _comment_data.Comment{
+		Comment:  "Viva RRQ",
+		UserID:   1,
+		ThreadID: 2,
+	}
+
 	if err := config.DB.Create(&user1).Error; err != nil {
 		panic(err)
 	}
@@ -73,6 +97,18 @@ func AutoMigrate() {
 	}
 
 	if err := config.DB.Create(&thread2).Error; err != nil {
+		panic(err)
+	}
+
+	if err := config.DB.Create(&comment1).Error; err != nil {
+		panic(err)
+	}
+
+	if err := config.DB.Create(&comment2).Error; err != nil {
+		panic(err)
+	}
+
+	if err := config.DB.Create(&comment3).Error; err != nil {
 		panic(err)
 	}
 }
