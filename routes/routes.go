838165105106d6b96027c63da600b1ea37006f4f
Ivan Muhammad Nizar <ivannizar@gmail.com>
2021-12-29 22:24:48 +0700

    init jwt and log middleware

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/dragranzer/capstone-BE-FGD/factory"
+	"github.com/dragranzer/capstone-BE-FGD/middleware"
 	"github.com/labstack/echo/v4"
 )
 
@@ -9,6 +10,7 @@ func Setup() *echo.Echo {
 	_presenter := factory.Init()
 	e := echo.New()
 
+	middleware.LogMidd(e)
 	e.POST("/user", _presenter.UserPresentation.Register)
 
 	return e
