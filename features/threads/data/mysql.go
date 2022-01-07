2eb59de8b7d9e9d505dc7a433d416a8616578d6f
Ivan Muhammad Nizar <ivannizar@gmail.com>
2022-01-07 17:24:37 +0700

    init threads repo layer

--- features/threads/data/mysql.go
+++ features/threads/data/mysql.go
@@ -1,1 +1,21 @@
 package data
+
+import (
+	"github.com/dragranzer/capstone-BE-FGD/features/threads"
+	"gorm.io/gorm"
+)
+
+type mysqlThreadRepository struct {
+	Conn *gorm.DB
+}
+
+func NewThreadRepository(conn *gorm.DB) threads.Data {
+	return &mysqlThreadRepository{
+		Conn: conn,
+	}
+}
+
+func (ur *mysqlThreadRepository) SelectThreadHome(data threads.Core) (resp []threads.Core, err error) {
+
+	return
+}
